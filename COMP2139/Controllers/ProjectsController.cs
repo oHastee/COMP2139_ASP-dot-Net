a98cf853de4efb1db8b3a0b92236d623f0570e7c
oHastee <oscarpiediaz@gmail.com>
2024-01-25 18:49:08 -0500

    “Lab”

---
+++ COMP2139/Controllers/ProjectsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP2139_Labs.Controllers;
+using COMP2139_Labs.Models;
+
+namespace COMP2139_Labs.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var projects = new List<Project>()
+            {
+                new Project {ProjectId = 1, Name = "Project 1", Description = "My First Project"} //we define a project
+            };
+
+            return View(projects); //we pass the project to the view
+        }
+
+
+
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Project project)
+        {
+            return RedirectToAction("Index");
+
+        }
+
+
+
+
+
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var project = new Project { ProjectId = id, Name = "Project " + id, Description = "Details of the project " + id };
+            return View(project);
+        }
+    }
+}
---
+++ COMP2139/Models/Project.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_Labs.Models
+{
+	public class Project
+
+	{
+		public int ProjectId { get; set; }
+
+
+		//[Required]
+
+		public /*required*/ string Name { get; set; }
+
+
+
+		public string? Description { get; set; } // ? this means the variable can be nullable
+
+
+        [DataType(DataType.Date)] 
+
+        public DateTime StartDate { get; set; }
+
+
+        [DataType(DataType.Date)] 
+
+        public DateTime EndDate { get; set; }
+
+
+
+		public string? Status { get; set; } // ? this means the variable can be nullable
+
+
+	}
+}
