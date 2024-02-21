7515847149949f50eae0f8336b5da294370ea364
oHastee <oscarpiediaz@gmail.com>
2024-02-21 12:59:20 -0500

    “Lab3”

--- COMP2139/Program.cs
+++ COMP2139/Program.cs
@@ -1,7 +1,16 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using COMP2139_Labs.Models;
+using COMP2139_Labs.Data;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure; // Ensure this using directive is present
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 36)))); // Adjust the version as necessary
 
 var app = builder.Build();
 
@@ -10,15 +19,22 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
+
+// Add the code to log database connection information
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    var connectionString = dbContext.Database.GetConnectionString();
+    app.Logger.LogInformation($"Connected to database: {connectionString}");
+}
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
